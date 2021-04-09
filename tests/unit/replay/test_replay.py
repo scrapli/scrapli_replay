b4a0b5e0b9678610160944c687021fac371915be
Carl Montanari <carl.r.montanari@gmail.com>
2021-04-08 17:10:35 -0700

    support many connections in a single test

--- tests/unit/replay/test_replay.py
+++ tests/unit/replay/test_replay.py
@@ -5,7 +5,12 @@ import pytest
 from ruamel.yaml import safe_load  # type: ignore
 
 from scrapli_replay.exceptions import ScrapliReplayException
-from scrapli_replay.replay.replay import ConnectionProfile, ReplayMode, ScrapliReplay
+from scrapli_replay.replay.replay import (
+    ConnectionProfile,
+    ReplayMode,
+    ScrapliReplay,
+    ScrapliReplayInstance,
+)
 
 
 def test_scrapli_replay_basic():
@@ -15,16 +20,14 @@ def test_scrapli_replay_basic():
     # even though we said record replay mode should be record since there is no session saved
     assert replay.replay_mode == ReplayMode.RECORD
     assert replay.replay_session == {}
-    assert isinstance(replay._read_log, BytesIO)
-    assert replay._write_log == []
+    assert replay.wrapped_instances == {}
     assert replay._patched_open is None
-    assert replay._wrapped_connection_profile is None
 
 
 def test_scrapli_replay_existing_session(fs):
     fs.create_file(
         f"{Path.cwd()}/test1.yaml",
-        contents="---\nsomekey: somevalue\n\ninteractions:\n  - something",
+        contents="---\nsomesession:\n  interactions:\n  - something",
     )
     # set replay mode to record, but will end up as replay as session exists!
     # pyfakefs does not like additional slashes in path it seems... so just set it to empty string
@@ -32,11 +35,9 @@ def test_scrapli_replay_existing_session(fs):
     assert str(replay.session_directory) == "."
     assert replay.session_name == "test1"
     assert replay.replay_mode == ReplayMode.REPLAY
-    assert replay.replay_session == {"interactions": ["something"], "somekey": "somevalue"}
-    assert isinstance(replay._read_log, BytesIO)
-    assert replay._write_log == []
+    assert replay.replay_session == {"somesession": {"interactions": ["something"]}}
+    assert replay.wrapped_instances == {}
     assert replay._patched_open is None
-    assert replay._wrapped_connection_profile is None
 
 
 def test_scrapli_replay_invalid_replay_mode():
@@ -47,7 +48,7 @@ def test_scrapli_replay_invalid_replay_mode():
 def test_session_exists(fs):
     fs.create_file(
         f"{Path.cwd()}/test1.yaml",
-        contents="---\nsomekey: somevalue\n\ninteractions:\n  - something",
+        contents="---\nsomesession:\n  interactions:\n  - something",
     )
     replay = ScrapliReplay(session_directory="", session_name="test1", replay_mode="record")
     assert replay._session_exists() is True
@@ -57,19 +58,18 @@ def test_session_exists(fs):
 
 def test_record_session_profile(scrapli_conn):
     replay = ScrapliReplay(session_name="test1", replay_mode="record")
-    replay._record_connection_profile(scrapli_conn=scrapli_conn)
-    assert replay._wrapped_connection_profile.host == "localhost"
-    assert replay._wrapped_connection_profile.port == 22
-    assert replay._wrapped_connection_profile.auth_username == ""
-    assert replay._wrapped_connection_profile.auth_password is False
-    assert replay._wrapped_connection_profile.auth_private_key == ""
-    assert replay._wrapped_connection_profile.auth_private_key_passphrase is False
-    assert replay._wrapped_connection_profile.auth_bypass is False
-    assert replay._wrapped_connection_profile.transport == "system"
+    conn_profile = replay.create_connection_profile(scrapli_conn=scrapli_conn)
+    assert conn_profile.host == "localhost"
+    assert conn_profile.port == 22
+    assert conn_profile.auth_username == ""
+    assert conn_profile.auth_password is False
+    assert conn_profile.auth_private_key == ""
+    assert conn_profile.auth_private_key_passphrase is False
+    assert conn_profile.auth_bypass is False
+    assert conn_profile.transport == "system"
 
 
 def test_common_replay_mode():
-    replay = ScrapliReplay(session_name="test1", replay_mode="record")
     connection_profile = {
         "host": "c3560",
         "port": 22,
@@ -81,6 +81,11 @@ def test_common_replay_mode():
         "transport": "asyncssh",
         "auth_secondary": False,
     }
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+
+    replay.replay_session = {}
     replay.replay_session["connection_profile"] = connection_profile
 
     replay.replay_session["interactions"] = [
@@ -96,7 +101,7 @@ def test_common_replay_mode():
         },
     ]
 
-    replay._wrapped_connection_profile = ConnectionProfile(**connection_profile)
+    replay.connection_profile = ConnectionProfile(**connection_profile)
     actual_device_outputs, actual_scrapli_inputs = replay._common_replay_mode()
 
     actual_device_outputs = list(actual_device_outputs)
@@ -107,8 +112,6 @@ def test_common_replay_mode():
 
 
 def test_common_replay_mode_exception():
-    replay = ScrapliReplay(session_name="test1", replay_mode="record")
-
     connection_profile = {
         "host": "c3560",
         "port": 22,
@@ -120,6 +123,10 @@ def test_common_replay_mode_exception():
         "transport": "asyncssh",
         "auth_secondary": False,
     }
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+
     replay._wrapped_connection_profile = ConnectionProfile(**connection_profile)
     replay.replay_session["connection_profile"] = connection_profile
     replay.replay_session["connection_profile"]["host"] = "blah"
@@ -137,11 +144,25 @@ async def test_setup_async_replay_mode(monkeypatch, scrapli_conn):
         return device_outputs, scrapli_inputs
 
     monkeypatch.setattr(
-        "scrapli_replay.replay.replay.ScrapliReplay._common_replay_mode", _common_replay_mode
+        "scrapli_replay.replay.replay.ScrapliReplayInstance._common_replay_mode",
+        _common_replay_mode,
     )
 
-    replay = ScrapliReplay(session_name="test1", replay_mode="record")
-    replay._setup_async_replay_mode(scrapli_conn=scrapli_conn)
+    connection_profile = {
+        "host": "c3560",
+        "port": 22,
+        "auth_username": "vrnetlab",
+        "auth_password": True,
+        "auth_private_key": "",
+        "auth_private_key_passphrase": False,
+        "auth_bypass": False,
+        "transport": "asyncssh",
+        "auth_secondary": False,
+    }
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+    replay.setup_async_replay_mode(scrapli_conn=scrapli_conn)
 
     # read off the channel, asserts we got the correct async method patched ++ we are returning
     # what we expect
@@ -161,24 +182,9 @@ def test_setup_replay_mode(monkeypatch, scrapli_conn):
         return device_outputs, scrapli_inputs
 
     monkeypatch.setattr(
-        "scrapli_replay.replay.replay.ScrapliReplay._common_replay_mode", _common_replay_mode
+        "scrapli_replay.replay.replay.ScrapliReplayInstance._common_replay_mode",
+        _common_replay_mode,
     )
-
-    replay = ScrapliReplay(session_name="test1", replay_mode="record")
-    replay._setup_replay_mode(scrapli_conn=scrapli_conn)
-
-    # read off the channel, asserts we got the correct async method patched ++ we are returning
-    # what we expect
-    assert scrapli_conn.channel.read() == b""
-    assert scrapli_conn.channel.read() == b"C3560CX#"
-
-    # we're out of stuff to read back, we should be having a bad day here
-    with pytest.raises(ScrapliReplayException):
-        scrapli_conn.channel.read()
-
-
-def test_serialize():
-    replay = ScrapliReplay(session_name="test1", replay_mode="record")
 
     connection_profile = {
         "host": "c3560",
@@ -191,13 +197,43 @@ def test_serialize():
         "transport": "asyncssh",
         "auth_secondary": False,
     }
-    replay._wrapped_connection_profile = ConnectionProfile(**connection_profile)
-    replay._read_log = BytesIO(
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+    replay.setup_replay_mode(scrapli_conn=scrapli_conn)
+
+    # read off the channel, asserts we got the correct async method patched ++ we are returning
+    # what we expect
+    assert scrapli_conn.channel.read() == b""
+    assert scrapli_conn.channel.read() == b"C3560CX#"
+
+    # we're out of stuff to read back, we should be having a bad day here
+    with pytest.raises(ScrapliReplayException):
+        scrapli_conn.channel.read()
+
+
+def test_serialize():
+    connection_profile = {
+        "host": "c3560",
+        "port": 22,
+        "auth_username": "vrnetlab",
+        "auth_password": True,
+        "auth_private_key": "",
+        "auth_private_key_passphrase": False,
+        "auth_bypass": False,
+        "transport": "asyncssh",
+        "auth_secondary": False,
+    }
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+
+    replay.read_log = BytesIO(
         b"Warning: Permanently added 'c3560,172.31.254.1' (RSA) to the list of known hosts.\nPassword: "
         b"\n\nC3560CX#\nC3560CX#terminal length 0\nC3560CX#terminal width 512\nC3560CX#show run | i "
         b"hostname\nhostname C3560CX\nC3560CX#\nC3560CX#"
     )
-    replay._write_log = [
+    replay.write_log = [
         ("VR-netlab9", True, 92),
         ("\n", False, 92),
         ("\n", False, 102),
@@ -212,118 +248,12 @@ def test_serialize():
         ("\n", False, 220),
     ]
 
-    actual_replay_session = replay._serialize()
+    replay_wrapper = ScrapliReplay()
+    replay_wrapper.wrapped_instances["fakesession"] = replay
+    actual_replay_session = replay_wrapper._serialize()
+
     assert actual_replay_session == {
-        "connection_profile": {
-            "host": "c3560",
-            "port": 22,
-            "auth_username": "vrnetlab",
-            "auth_password": True,
-            "auth_private_key": "",
-            "auth_private_key_passphrase": False,
-            "auth_bypass": False,
-            "transport": "asyncssh",
-            "auth_secondary": False,
-        },
-        "interactions": [
-            {
-                "channel_output": "Warning: Permanently added 'c3560,172.31.254.1' (RSA) to the list of known hosts.\nPassword: ",
-                "expected_channel_input": "REDACTED",
-                "expected_channel_input_redacted": True,
-            },
-            {
-                "channel_output": "",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\n\nC3560CX#",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\nC3560CX#",
-                "expected_channel_input": "terminal length 0",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "terminal length 0",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\nC3560CX#",
-                "expected_channel_input": "terminal width 512",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "terminal width 512",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\nC3560CX#",
-                "expected_channel_input": "show run | i hostname",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "show run | i hostname",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\nhostname C3560CX\nC3560CX#",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "\nC3560CX#",
-                "expected_channel_input": "exit",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "",
-                "expected_channel_input": "\n",
-                "expected_channel_input_redacted": False,
-            },
-            {
-                "channel_output": "",
-                "expected_channel_input": None,
-                "expected_channel_input_redacted": False,
-            },
-        ],
-    }
-
-
-def test_save(fs):
-    replay = ScrapliReplay(session_directory="", session_name="test1", replay_mode="record")
-
-    connection_profile = {
-        "host": "c3560",
-        "port": 22,
-        "auth_username": "vrnetlab",
-        "auth_password": True,
-        "auth_private_key": "",
-        "auth_private_key_passphrase": False,
-        "auth_bypass": False,
-        "transport": "asyncssh",
-        "auth_secondary": False,
-    }
-    replay._wrapped_connection_profile = ConnectionProfile(**connection_profile)
-    replay._read_log = BytesIO(
-        b"Warning: Permanently added 'c3560,172.31.254.1' (RSA) to the list of known hosts.\nPassword: "
-        b"\n\nC3560CX#\nC3560CX#"
-    )
-    replay._write_log = [
-        ("VR-netlab9", True, 92),
-        ("\n", False, 92),
-        ("\n", False, 102),
-    ]
-    replay._save()
-
-    with open(f"./test1.yaml", "r") as f:
-        loaded = safe_load(f)
-        assert loaded == {
+        "fakesession": {
             "connection_profile": {
                 "host": "c3560",
                 "port": 22,
@@ -353,8 +283,126 @@ def test_save(fs):
                 },
                 {
                     "channel_output": "\nC3560CX#",
+                    "expected_channel_input": "terminal length 0",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "terminal length 0",
+                    "expected_channel_input": "\n",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "\nC3560CX#",
+                    "expected_channel_input": "terminal width 512",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "terminal width 512",
+                    "expected_channel_input": "\n",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "\nC3560CX#",
+                    "expected_channel_input": "show run | i hostname",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "show run | i hostname",
+                    "expected_channel_input": "\n",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "\nhostname C3560CX\nC3560CX#",
+                    "expected_channel_input": "\n",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "\nC3560CX#",
+                    "expected_channel_input": "exit",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "",
+                    "expected_channel_input": "\n",
+                    "expected_channel_input_redacted": False,
+                },
+                {
+                    "channel_output": "",
                     "expected_channel_input": None,
                     "expected_channel_input_redacted": False,
                 },
             ],
+        }
+    }
+
+
+def test_save(fs):
+    connection_profile = {
+        "host": "c3560",
+        "port": 22,
+        "auth_username": "vrnetlab",
+        "auth_password": True,
+        "auth_private_key": "",
+        "auth_private_key_passphrase": False,
+        "auth_bypass": False,
+        "transport": "asyncssh",
+        "auth_secondary": False,
+    }
+    replay = ScrapliReplayInstance(
+        replay_mode=ReplayMode.RECORD, connection_profile=ConnectionProfile(**connection_profile)
+    )
+
+    replay._wrapped_connection_profile = ConnectionProfile(**connection_profile)
+    replay.read_log = BytesIO(
+        b"Warning: Permanently added 'c3560,172.31.254.1' (RSA) to the list of known hosts.\nPassword: "
+        b"\n\nC3560CX#\nC3560CX#"
+    )
+    replay.write_log = [
+        ("VR-netlab9", True, 92),
+        ("\n", False, 92),
+        ("\n", False, 102),
+    ]
+
+    replay_wrapper = ScrapliReplay(session_directory="", session_name="test1", replay_mode="record")
+    replay_wrapper.wrapped_instances["fakesession"] = replay
+    replay_wrapper._save()
+
+    with open(f"./test1.yaml", "r") as f:
+        loaded = safe_load(f)
+        assert loaded == {
+            "fakesession": {
+                "connection_profile": {
+                    "host": "c3560",
+                    "port": 22,
+                    "auth_username": "vrnetlab",
+                    "auth_password": True,
+                    "auth_private_key": "",
+                    "auth_private_key_passphrase": False,
+                    "auth_bypass": False,
+                    "transport": "asyncssh",
+                    "auth_secondary": False,
+                },
+                "interactions": [
+                    {
+                        "channel_output": "Warning: Permanently added 'c3560,172.31.254.1' (RSA) to the list of known hosts.\nPassword: ",
+                        "expected_channel_input": "REDACTED",
+                        "expected_channel_input_redacted": True,
+                    },
+                    {
+                        "channel_output": "",
+                        "expected_channel_input": "\n",
+                        "expected_channel_input_redacted": False,
+                    },
+                    {
+                        "channel_output": "\n\nC3560CX#",
+                        "expected_channel_input": "\n",
+                        "expected_channel_input_redacted": False,
+                    },
+                    {
+                        "channel_output": "\nC3560CX#",
+                        "expected_channel_input": None,
+                        "expected_channel_input_redacted": False,
+                    },
+                ],
+            }
         }
